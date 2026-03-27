425391213407851051b9a250ccf1efb27d395c3c
Sayyed Ali-dev <alisayedali618@gmail.com>
2026-03-27 10:26:13 +0500

    Fix formatting of register_activation_hook line

--- contact-form.php
+++ contact-form.php
@@ -7,16 +7,12 @@ Version: 1.2
 Author: Sayyed Ali
 */
 
-
 // Security
 if (!defined('ABSPATH')) {
     exit;
 }
 
-
-
 // Include files
 require_once plugin_dir_path(__FILE__) . 'includes/functions.php';
 
-register_activation_hook(__FILE__, 'create_form_table') 
-;
+register_activation_hook(__FILE__, 'create_form_table') ;
